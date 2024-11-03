7e9bc67db55d6a214330f1e1d2cbd9876a317742
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-11-03 18:07:41 +0530

    currency converter project

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import "./components/InputBox"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='min-w-full h-screen ' style={{backgroundColor: "blue"}}>
+
+    </div>
+    </>
+  )
+}
+
+export default App
